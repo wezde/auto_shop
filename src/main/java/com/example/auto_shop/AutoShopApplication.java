bfb2a533c0a1982f6995cf00132804c98a1c61b5
Aleksandr Pudul <wezdewlez@gmail.com>
2024-09-09 19:43:53 +0300

    test commit

---
+++ src/main/java/com/example/auto_shop/AutoShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auto_shop/AutoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
